2a68c366b4a9040a55b665ebc9ef7fc96928905d
jmm <1320381667@qq.com>
2019-01-02 14:31:26 +0800

    add subsets

---
+++ math/subsets.py
@@ -0,0 +1,48 @@
+class Solution:
+    """
+    @param nums: A set of numbers
+    @return: A list of lists
+    """
+
+    def subsets_recursive(self, nums):
+        if len(nums) == 0:
+            return [[]]
+
+        subsets_list = []
+        for subset in self.subsets_recursive(nums[:-1]):
+            if not len(subset) == 0:
+                subset_cp = subset.copy()
+                subset_cp.append(nums[-1])
+                subsets_list.append(sorted(subset_cp))
+        subsets_list.append([nums[-1]])
+
+        return subsets_list + self.subsets_recursive(nums[:-1])
+
+    def subsets(self, nums):
+        subsets = [[]]
+        if len(nums) == 0:
+            return subsets
+
+        i = 0
+        while i < len(nums):
+            subsets_list = []
+            for subset in subsets:
+                if not subset == []:
+                    subset_cp = subset.copy()
+                    subset_cp.append(nums[i])
+                    subsets_list.append(sorted(subset_cp))
+            subsets_list.append([nums[i]])
+            subsets += subsets_list
+            i += 1
+
+        return subsets
+
+
+if __name__ == '__main__':
+    data = [1, 2, 3]
+    s = Solution()
+    print(s.subsets(data))
+    print(len(s.subsets(data)))
+
+    print(s.subsets_recursive(data))
+    print(len(s.subsets_recursive(data)))
